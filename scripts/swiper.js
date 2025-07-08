6fa0c4d90565b2fcfc8c77d3bc3cc39ea80e11a8
surim95 <cheasulim2731@gmail.com>
2025-07-08 18:32:05 +0900

    그랑핸드 데스크탑

--- scripts/swiper.js
+++ scripts/swiper.js
@@ -1,6 +1,6 @@
-const shopSwiper = new Swiper('.product_swiper',{
-    /* autoplay:{delay:2000,}, */
+/* const shopSwiper = new Swiper('.product_swiper',{
+    /* autoplay:{delay:2000,},
     loop:true,
     direction:'horizion',
-    slidesPerView: 'auto',
-});
+    slidesPerView: 3,
+}); */
