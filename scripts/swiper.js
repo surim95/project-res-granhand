24dde38b3e63fa3f58c8012e741aa7c9a77c0b37
surim95 <cheasulim2731@gmail.com>
2025-07-21 19:29:24 +0900

    그랑핸드 작업중

--- scripts/swiper.js
+++ scripts/swiper.js
@@ -25,46 +25,31 @@ const setSwiper = new Swiper ('.set_swiper',{
 
 const journalSwiper = new Swiper ('.journal_img',{
     //autoplay:{delay:10000},
-    /* effect: 'fade', */
+    effect: 'creative',
     slidesPerView: 1,
-    spaceBetween: -1000,
-    loop: true,
+    loopAdditionalSlides: 1,
+    rewind: true,
     navigation:{
         nextEl:'.journal_img > .swiper-button-next',
         prevEl:'.journal_img > .swiper-button-prev',
     },
+    creativeEffect: {
+        limitProgress:2,
+        prev: {
+            translate: [-48, 48, -2],
+            opacity:0.3,
+        },
+        next: {
+            translate: [-24, 24, -1],
+            opacity:0.6,
+        },
+    },
     on: {
-        slideChange: function() {
-            if (this.realIndex == 0){
-                console.log('0입니다.');
-                this.slides[0].style.zIndex = '50';
-                this.slides[1].style.zIndex = '49';
-                this.slides[2].style.zIndex = '48';
-                this.slides[1].style.transform = 'translateY(0)';
-                this.slides[1].style.transform = 'translateY(-20px)';
-                this.slides[2].style.transform = 'translateY(-40px)';
-            } else if (this.realIndex == 1) {
-                console.log('1입니다.');
-                this.slides[1].style.zIndex = '50';
-                this.slides[2].style.zIndex = '49';
-                this.slides[0].style.zIndex = '48';
-                this.slides[1].style.transform = 'translateY(0)';
-                this.slides[2].style.transform = 'translateY(-20px)';
-                this.slides[0].style.transform = 'translateY(-40px)';
-            } else {
-                console.log('2입니다.');
-                this.slides[2].style.zIndex = '50';
-                this.slides[0].style.zIndex = '49';
-                this.slides[1].style.zIndex = '48';
-                this.slides[2].style.transform = 'translateY(0)';
-                this.slides[0].style.transform = 'translateY(-20px)';
-                this.slides[1].style.transform = 'translateY(-40px)';
-            }
-            /* console.log(this.activeIndex); */
+        slideChange: function () {
+            console.log(this.realIndex);
         }
-    }
+    },
 })
-
 
 const indexSwiper = new Swiper ('.index_contents', {
     //autoplay:{delay:10000},
